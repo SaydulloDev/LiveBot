86fe39f6e6bd15b66958e8bf756b9f90c8572711
Tukhtasinov-Saydullo <tukhtasinov.saydullo@yahoo.com>
2023-06-23 13:41:22 +0500

    Database file

---
+++ db.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+
+def add_user(chat_id, first_name, last_name, username=None):
+    try:
+        with sqlite3.connect('db.sqlite3') as connection:
+            cursor = connection.cursor()
+            if username is not None:
+                cursor.execute('INSERT INTO users (chat_id,username, first_name, last_name) VALUES (?,?,?,?)',
+                               (chat_id, username, first_name, last_name))
+            else:
+                cursor.execute('INSERT INTO users (chat_id,first_name,last_name) VALUES (?,?,?)',
+                               (chat_id, first_name, last_name))
+    except Exception as e:
+        return f'Failed to add user {e}'
+
+
+def get_user(chat_id):
+    try:
+        with sqlite3.connect('db.sqlite3') as connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT chat_id FROM users WHERE chat_id = ?', (chat_id,))
+            result = cursor.fetchall()
+            if not result:
+                return False
+            else:
+                return True
+    except Exception as e:
+        return f'Failed to get user {e}'
+
+
+def get_full_info():
+    try:
+        with sqlite3.connect('db.sqlite3') as connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT first_name, chat_id FROM users')
+            result = cursor.fetchall()
+            return result
+    except Exception as e:
+        return f'Error {e}'
